032a6f4fdf7d7b433772b8e6e634920b51630f53
Shinia <113818197+Shinia-Gupta@users.noreply.github.com>
2026-04-16 20:54:09 +0530

    Add: course-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-5.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer,List<Integer>> graph=new HashMap<>();
+        // Map<Integer,Integer> indegreeMap=new HashMap<>();
+        int[] indegreeMap=new int[numCourses];
+        List<Integer> resultList=new ArrayList<>();
+        int[] result=new int[numCourses];
+        //initialize maps
+        for(int i=0;i<numCourses;i++){
+          graph.computeIfAbsent(i,x->new ArrayList<>());
+          // indegreeMap.put(i,0);
+        }
+        Arrays.fill(indegreeMap,0);
+
+        //create directed edges and calculate indegrees for each node
+        for(int prereq[]:prerequisites){
+          int a=prereq[0],b=prereq[1];
+          graph.get(b).add(a);
+          // indegreeMap.put(a,indegreeMap.get(a)+1);
+          indegreeMap[a]++;
+        }
+
+        //initialize queue
+        Queue<Integer> q=new LinkedList<>();
+        // for(Map.Entry<Integer,Integer> entry:indegreeMap.entrySet()){
+        //   if(entry.getValue()==0) q.add(entry.getKey());
+        // }
+        for(int i=0;i<indegreeMap.length;i++){
+          if(indegreeMap[i]==0) q.add(i);
+        }
+
+        //process nodes
+        while(!q.isEmpty()){
+          int node=q.poll();
+          resultList.add(node);
+          for(int neighbor:graph.get(node)){
+          // indegreeMap.put(neighbor,indegreeMap.get(neighbor)-1);
+          indegreeMap[neighbor]--;
+          // if(indegreeMap.get(neighbor)==0) q.add(neighbor);
+          if(indegreeMap[neighbor]==0) q.add(neighbor);
+          }
+        }
+        int index=0;
+        for(int node:resultList){
+            result[index++]=node;
+        }
+        return resultList.size()==graph.size()?result:new int[0];
+
+
+    }
+}
