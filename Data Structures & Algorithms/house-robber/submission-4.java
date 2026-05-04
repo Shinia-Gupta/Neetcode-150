4f271a33c6b851585e56f0e09a5c52691253f960
Shinia <113818197+Shinia-Gupta@users.noreply.github.com>
2026-05-04 21:11:16 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int dp[]=new int[nums.length];
+        if(nums.length<=1) return nums[0];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+
+        for(int i=2;i<nums.length;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+
+        // ye max toh humne for ecah house ya till each index, loop mei hi store krlkia tha, toh ismei max dekhne ki zarurat nhi h
+        return dp[nums.length-1];
+    }
+}
